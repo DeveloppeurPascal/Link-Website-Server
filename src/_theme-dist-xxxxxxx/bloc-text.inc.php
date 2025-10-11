c26405210de8327bd4963ac00aba190920670ded
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2025-10-11 19:28:26 +0200

    added the "text" block PHP+HTML code for default theme

---
+++ src/_theme-dist-xxxxxxx/bloc-text.inc.php
@@ -0,0 +1,16 @@
+<?php
+// {
+// 	type : "text"
+// 	content [
+// 		lang
+// 		text
+// 		is_public (boolean)
+// 	]
+// 	is_public (boolean)
+// }
+
+	if (isset($CurrentBlock) && is_object($CurrentBlock) && isset($CurrentBlock->type) && ("text" == $CurrentBlock->type)) {
+		if (false !== ($content = (isset($CurrentBlock->content) && is_object($CurrentBlock->content))?GetObjectByLanguage($CurrentBlock->content,$LanguageISOCode))) {
+			print("<p".$level.(($LanguageISOCode!=$content->lang)?" lang=\"".$content->lang."\"":"").">".$content->text."</p>");
+		}
+	}
