a24cf6637b851bfaa73942395d4256de55ecee17
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2025-10-11 19:52:37 +0200

    added <br> tags to the text for each new line

--- src/_theme-dist-xxxxxxx/bloc-text.inc.php
+++ src/_theme-dist-xxxxxxx/bloc-text.inc.php
@@ -11,6 +11,6 @@
 
 	if (isset($CurrentBlock) && is_object($CurrentBlock) && isset($CurrentBlock->type) && ("text" == $CurrentBlock->type)) {
 		if (false !== ($content = (isset($CurrentBlock->content) && is_object($CurrentBlock->content))?GetObjectByLanguage($CurrentBlock->content,$LanguageISOCode))) {
-			print("<p".(($LanguageISOCode!=$content->lang)?" lang=\"".$content->lang."\"":"").">".$content->text."</p>");
+			print("<p".(($LanguageISOCode!=$content->lang)?" lang=\"".$content->lang."\"":"").">".nl2br($content->text)."</p>");
 		}
 	}
