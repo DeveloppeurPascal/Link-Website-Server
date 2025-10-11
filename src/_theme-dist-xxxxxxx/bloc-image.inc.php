dcede87a516b4f325cecc3e166f95b012a1cd17c
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2025-10-11 19:37:44 +0200

    added the "image" block PHP+HTML code for default theme

---
+++ src/_theme-dist-xxxxxxx/bloc-image.inc.php
@@ -0,0 +1,25 @@
+<?php
+// {
+// 	type : "image"
+// 	content [
+// 		lang
+// 		text
+// 		url_image
+// 		url
+// 		is_public (boolean)
+// 	]
+// 	is_public (boolean)
+// }
+
+	if (isset($CurrentBlock) && is_object($CurrentBlock) && isset($CurrentBlock->type) && ("image" == $CurrentBlock->type)) {
+		if (false !== ($content = (isset($CurrentBlock->content) && is_object($CurrentBlock->content))?GetObjectByLanguage($CurrentBlock->content,$LanguageISOCode))) {
+			print("<p".(($LanguageISOCode!=$content->lang)?" lang=\"".$content->lang."\"":"").">");
+			$WithLink = isset($content->url) && (!empty($content->url));
+			if ($WithLink)
+				print("<a href=\"".$content->url."\">");
+			print("<img".((isset($content->url_image) && (!empty($content->url_image)))?" src=\"".$content->url_image."\"":"").((isset($content->text) && (!empty($content->text)))?" alt=\"".$content->text."\"":"").">");
+			if ($WithLink)
+				print("</a>");
+			print("</p>");
+		}
+	}
